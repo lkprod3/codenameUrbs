87b6938bf11786f7a0cf5dbf5bd6903f220843ca
Gabriel Crisóstomo <lkprod2@gmail.com>
2018-11-21 23:08:29 -0200

    all I had time to do

--- js/urbs.js
+++ js/urbs.js
@@ -8,8 +8,16 @@
 //	});
 //});
 
+
+
+
+
 // Seta indo pra cima e Logo "sumindo"
 $(document).ready(function () {
+  $("#menu").hide();
+  $("#episodios").hide();
+  $("#noticias").hide();
+
   $( "#seta-splash" ).stop(true, true).delay(500).animate({
     top: "-2000%",
     rotation: "0.01"
@@ -20,7 +28,41 @@ $(document).ready(function () {
   $( "#logo-splash").stop(true, true).delay(650).animate({
   	opacity: 0,
   }, 200, "linear", function() {
+  $("#noticias").show("slide", { direction: "down" }, 320);
   })
+
+});
+
+function menutoggle() {
+  if ( $("#episodios").is(":visible")) {
+  $("#episodios").hide("slide", { direction: "down" }, 200);
+  }
+  if ( $("#noticias").is(":visible")) {
+    $("#noticias").hide("slide", { direction: "down" }, 200);
+  }
+  if ($("#menu").is(":hidden")) {
+    $('#menu').show("slide", { direction: "up" }, 320);
+  }
+  else if ($("#menu").is(":visible")) {
+    $('#menu').hide("slide", { direction: "up" }, 200);
+    $("#noticias").show("slide", { direction: "down" }, 320);
+  }
+  }
+
+  function gotoEpisodios() {
+    $('#menu').hide();
+    $("#episodios").show("slide", { direction: "left" }, 200);
+    }
+    
+
+  function gotoHome() {
+    $('#menu').hide();
+    $("#noticias").show("slide", { direction: "left" }, 200);
+  }
+
+$(".card_1").click(function() {
+    $("#noticias").hide("slide", { direction: "right" }, 200);
+    $("#episodios").show("slide", { direction: "left" }, 200);
 });
 
 // This is the menu button, animated
@@ -28,124 +70,11 @@ function myFunction(x) {
     x.classList.toggle("change");
 }
 
-
-// $(function () {
-
-//   checkboxes.click(function () {
-//       // The checkboxes in our app serve the purpose of filters.
-//       // Here on every click we add or remove filtering criteria from a filters object.
-
-//       // Then we call this function which writes the filtering criteria in the url hash.
-//       createQueryHash(filters);
-//   });
-
-//   $.getJSON( !"cards_home.json", function( data ) {
-//       // Get data about our products from products.json.
-
-//       // Call a function that will turn that data into HTML.
-//       generateAllProductsHTML(data);
-
-//       // Manually trigger a hashchange to start the app.
-//       $(window).trigger('hashchange');
-//   });
-
-//   $(window).on('hashchange', function(){
-//       // On every hash change the render function is called with the new hash.
-//       // This is how the navigation of our app happens.
-//       render(decodeURI(window.location.hash));
-//   });
-
-//   function render(url) {
-//       // This function decides what type of page to show 
-//       // depending on the current url hash value.
-//   }
-//   function generateAllProductsHTML(data){
-//       // Uses Handlebars to create a list of products using the provided data.
-//       // This function is called only once on page load.
-//   }
-//   function renderProductsPage(data){
-//       // Hides and shows products in the All Products Page depending on the data it recieves.
-//   }
-//   function renderSingleProductPage(index, data){
-//       // Shows the Single Product Page with appropriate data.
-//   }
-//   function renderFilterResults(filters, products){
-//       // Crates an object with filtered products and passes it to renderProductsPage.
-//       renderProductsPage(results);
-//   }
-//   function renderErrorPage(){
-//       // Shows the error page.
-//   }
-//   function createQueryHash(filters){
-//       // Get the filters object, turn it into a string and write it into the hash.
-//   }
-// });
-
-// function render(url) {
-
-//   // Get the keyword from the url.
-//   var temp = url.split('/')[0];
-
-//   // Hide whatever page is currently shown.
-//   $('.main-content .page').removeClass('visible');
-
-//   var map = {
-
-//       // The Homepage.
-//       '': function() {
-
-//           // Clear the filters object, uncheck all checkboxes, show all the products
-//           filters = {};
-//           checkboxes.prop('checked',false);
-
-//           renderProductsPage(products);
-//       },
-
-//       // Single Products page.
-//       '#product': function() {
-
-//           // Get the index of which product we want to show and call the appropriate function.
-//           var index = url.split('#product/')[1].trim();
-
-//           renderSingleProductPage(index, products);
-//       },
-
-//       // Page with filtered products
-//       '#filter': function() {
-
-//           // Grab the string after the '#filter/' keyword. Call the filtering function.
-//           url = url.split('#filter/')[1].trim();
-
-//           // Try and parse the filters object from the query string.
-//           try {
-//               filters = JSON.parse(url);
-//           }
-//           // If it isn't a valid json, go back to homepage ( the rest of the code won't be executed ).
-//           catch(err) {
-//               window.location.hash = '#';
-//           }
-
-//           renderFilterResults(filters, products);
-//       }
-
-//   };
-
-//   // Execute the needed function depending on the url keyword (stored in temp).
-//   if(map[temp]){
-//       map[temp]();
-//   }
-//   // If the keyword isn't listed in the above - render the error page.
-//   else {
-//       renderErrorPage();
-//   }
-
-// }
-
 $(function json_cards() {
   var cards_home = [];
   $.getJSON('json/cards_home.json', function(data) {
       $.each(data, function(i, f) {
-         var card_home = '<a href="' + f.pointer + '" class="text-decor-none"><div class="card mx-auto mb-5 w-98" data-index="' + f.id + '"><img class="card-img-top" src="' + f.imgcard + '" alt="Card image cap"><div class="card-body"><p class="card-text text-center font-weight-bold">' + f.titulo + '</p></div></a>'
+         var card_home = '<a href="' + f.pointer + '" class="text-decor-none card_' + f.id + '"><div class="card mx-auto mb-5 w-98" data-index="' + f.id + '"><img class="card-img-top" src="' + f.imgcard + '" alt="Card image cap"><div class="card-body"><p class="card-text text-center font-weight-bold">' + f.titulo + '</p></div></a>'
           $(card_home).appendTo("#noticias");
     });
   });
@@ -169,13 +98,23 @@ if ( $(window).width() > 70) {
   mywindow.scroll(function () {
       newscroll = mywindow.scrollTop();
       if (newscroll > mypos && !up) {
-          $('.slidd').stop().slideUp(400);
+          $('.slidd').stop().slideUp(200);
           up = !up;
           console.log(up);
       } else if(newscroll < mypos && up) {
-          $('.slidd').stop().slideDown(300);
+          $('.slidd').stop().slideDown(90);
           up = !up;
       }
       mypos = newscroll;
   });
   }
+
+  $(function ( ) {
+    $(window).on('hashchange', function(){
+      // On every hash change the render function is called with the new hash.
+      // This is how the navigation of our app happens.
+      render(decodeURI(window.location.hash));
+  });
+  function render(url) {
+  }
+  })
